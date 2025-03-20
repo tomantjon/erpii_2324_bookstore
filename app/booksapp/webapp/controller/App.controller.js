6abaec9ec2e0c7892a016a65d3c3052710fbaa61
Tom Antjon <tom.antjon@hogent.be>
2025-03-20 12:22:49 +0100

    [APP] Generated Basic FreeStyle App

---
+++ app/booksapp/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("my.booksapp.controller.App", {
+        onInit() {
+        }
+    });
+});
